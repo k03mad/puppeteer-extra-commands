58810c73e920e447ea7b77a35af9b0b3e73dba7a
Kirill Molchanov <k03.mad@gmail.com>
2018-10-10 03:27:06 +0300

    Update scrollWindowBy.js

--- commands/scrollWindowBy.js
+++ commands/scrollWindowBy.js
@@ -1,6 +1,6 @@
 /**
  * Scroll page by px count
- * @param {Object} page browser page
- * @param {Number} scroll px count for page scroll
+ * @param {Object} page of puppeteer
+ * @param {Number{}} x,y px count for page scroll
  */
 module.exports = (page, {x = 0, y = 0} = {}) => page.evaluate((scrollX, scrollY) => window.scrollBy(scrollX, scrollY), x, y);
