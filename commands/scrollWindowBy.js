5fce31e5ba6e5a78e37803ae9aa51ba0247884b8
k03mad <k03.mad@gmail.com>
2019-02-25 16:16:05 +0300

    sml fix

--- commands/scrollWindowBy.js
+++ commands/scrollWindowBy.js
@@ -7,6 +7,7 @@
  * @param {number} obj.x px count for horizontal page scroll
  * @param {number} obj.y px count for vertical page scroll
  */
-module.exports = async (page, {x = 0, y = 0} = {}) => {
-    await page.evaluate((scrollX, scrollY) => window.scrollBy(scrollX, scrollY), x, y);
-};
+module.exports = (page, {x = 0, y = 0} = {}) => page.evaluate(
+    (scrollX, scrollY) => window.scrollBy(scrollX, scrollY),
+    x, y
+);
